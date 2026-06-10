dbd9a17fb829fb268c53858d453927c6d007d5be
Abhinav <abhinavravi.shankar.s.112@kalvium.community>
2026-06-10 11:54:29 +0530

    567

---
+++ 567.java
@@ -0,0 +1,31 @@
+// https://leetcode.com/problems/permutation-in-string/description/
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+    int n = s1.length();
+    int m = s2.length();
+
+    if (n > m)
+        return false;
+
+    int[] s1Count = new int[26];
+    int[] windowCount = new int[26];
+
+    for (int i = 0; i < n; i++) {
+        s1Count[s1.charAt(i) - 'a']++;
+        windowCount[s2.charAt(i) - 'a']++;
+    }
+
+    if (Arrays.equals(s1Count, windowCount))
+        return true;
+
+    for (int i = n; i < m; i++) {
+        windowCount[s2.charAt(i) - 'a']++;
+        windowCount[s2.charAt(i - n) - 'a']--;
+
+        if (Arrays.equals(s1Count, windowCount))
+            return true;
+    }
+
+    return false;
+    }
+}
