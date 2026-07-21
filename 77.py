0209a0a36db7e1ca74f4c672d83562121bb61b77
ABHINAV RAVI SHANKAR <abhinavravi.shankar.s.112@kalvium.community>
2026-07-21 17:56:13 +0530

    Add combination generation method in Solution class
    Implement backtracking to generate combinations of numbers.

---
+++ 77.py
@@ -0,0 +1,17 @@
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        res = []
+        curr = []
+
+        def backtrack(start):
+            if len(curr) == k:
+                res.append(curr[:])
+                return
+
+            for i in range(start, n + 1):
+                curr.append(i)
+                backtrack(i + 1)
+                curr.pop()
+
+        backtrack(1)
+        return res
