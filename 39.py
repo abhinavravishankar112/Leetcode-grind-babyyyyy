7dc0c174c55cd782918a801325196fcd1f423e9b
ABHINAV RAVI SHANKAR <abhinavravi.shankar.s.112@kalvium.community>
2026-07-21 17:44:35 +0530

    Implement combinationSum function using backtracking

---
+++ 39.py
@@ -0,0 +1,21 @@
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        res = []
+        curr = []
+
+        def backtrack(i, total):
+            if total == target:
+                res.append(curr.copy())
+                return
+            
+            if i >= len(candidates) or total > target:
+                return
+            
+            curr.append(candidates[i])
+            backtrack(i, total + candidates[i])
+            curr.pop()
+
+            backtrack(i + 1, total)
+        
+        backtrack(0, 0)
+        return res
